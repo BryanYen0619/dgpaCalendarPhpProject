22cfb40ea52d6fb18893733539c0c6e73ce17b6f
Bryan.Yen <bryan.yen@yfyshop.com>
2020-04-08 16:42:56 +0800

    # 取得撥款日(有計算人事行政局日曆)

---
+++ getAppropriationDate.php
@@ -0,0 +1,110 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+date_default_timezone_set("Asia/Taipei");
+
+function getAppropriationDate($inputDate) {
+    // 取得休假列表
+    $holidayList = getHolidayList($inputDate);
+    
+    $date = date_create($inputDate);
+    
+    $dateYearMonth = date_format($date, 'Ym');
+    $yearCount = date_format($date, 'Y');
+    $monthCount = date_format($date, 'm');
+    $dayCount = date_format($date, 'd');
+    if ($dayCount >= 1 && $dayCount <= 10) {
+        $new_date = date_create($dateYearMonth . "22"); 
+    } else if ($dayCount >= 11 && $dayCount <= 20) {        
+        $dateYearMonth = date("Ym", mktime(0, 0, 0, $monthCount + 1, 1, $yearCount));
+        $new_date = date_create($dateYearMonth . "02");  
+    } else if ($dayCount >= 21 && $dayCount <= 31) {
+        $dateYearMonth = date("Ym", mktime(0, 0, 0, $monthCount + 1, 1, $yearCount));
+        $new_date = date_create($dateYearMonth . "12");
+    } else {
+        $new_date = -1;
+    }
+    
+    $appropriationDate = date_format($new_date, 'Ymd');
+    
+    // 判斷請求日期是否為假日
+    $checkCurrentDateIsChtHoliday = isChtHoliday($holidayList, $appropriationDate);
+    // 找下一個工作日
+    if ($checkCurrentDateIsChtHoliday) {
+        while (1) {
+            $appropriationDate = date('Ymd', strtotime("$appropriationDate +1 Days"));
+            $check = isChtHoliday($holidayList, $appropriationDate);
+            if (!$check) {
+                break;
+            }
+        }
+    }
+
+    return $appropriationDate;
+}
+
+function getHolidayList($inputDate) {
+     // 日期建立
+    $beginDate = date_create($inputDate);
+    $endDate = date_create($inputDate)->modify('last day of +1 month');
+    
+    // 日期時區轉換 Asia -> UTC  
+    $beginDate->setTimezone(new DateTimeZone('UTC'));
+    $endDate->setTimezone(new DateTimeZone('UTC'));
+    
+    // 日期取Timestamp
+    $beginDateTimestamp = $beginDate->getTimestamp();
+    $endDateTimestamp = $endDate->getTimestamp();
+    
+    // SQL init
+    require_once('Connections/link.php');
+    // SQL Select table
+    $db_table_name = 'calendar_table';
+
+
+    // 取得資料
+    $selectSql = "SELECT date FROM $db_table_name "
+            . "WHERE UNIX_TIMESTAMP(STR_TO_DATE(date, '%Y/%m/%d')) >= $beginDateTimestamp "
+            . "AND UNIX_TIMESTAMP(STR_TO_DATE(date, '%Y/%m/%d')) < $endDateTimestamp ";
+    
+    if ($isHightPhpVersion) {
+        $selectSqlQuery = mysqli_query($mysql, $selectSql);
+    } else {
+        $selectSqlQuery = mysql_query($selectSql);
+    }
+
+    if ($selectSqlQuery) {
+        $rows = array();
+        if ($isHightPhpVersion) {
+            $selectSqlQuery = mysqli_query($mysql, $selectSql);
+            while ($row = mysqli_fetch_assoc($selectSqlQuery)) {
+                    $rows[] = $row;
+                }
+        } else {
+            $selectSqlQuery = mysql_query($selectSql);
+            while ($row = mysql_fetch_assoc($selectSqlQuery)) {
+                    $rows[] = $row;
+                }
+        }
+    } 
+    
+    return $rows;
+}
+
+function isChtHoliday($holidayList, $inputdate) {
+    for ($i = sizeof($holidayList) - 1; $i > -1; $i--) {
+        //         echo "[".$i."]".$chtHolidayData[$i]['date']."\n";
+
+        $holidayDate = date_create($holidayList[$i]);
+        if ($inputdate == $holidayDate) {
+            return true;
+        }
+    }
+
+    return false;
+}
